6d7271f4bd99d1d7d7ed89fc04f226dc683c2234
Tasnim A. <tasu2301@gmail.com>
2024-08-27 20:23:31 +0600

    export methods, error handling, html tag

--- Basics/exportfile.py
+++ Basics/exportfile.py
@@ -7,12 +7,12 @@ list_b = [2, 5, 1, 8]
 list_dict = {'a': list_a, 'b': list_b}
 
 # Method one to export data
-# with open('data.txt', 'w') as file:
-#     file.write('File scraped successfully')
+with open('Basics/data.txt', 'w') as file:
+    file.write('File scraped successfully')
 
 
 # Method two to export data with pandas
 df_list = pd.DataFrame.from_dict(list_dict)
 # convert the dataframe to a csv file - to remove the index number we can make it false
-df_list.to_csv('data.csv', index=False)
+df_list.to_csv("Basics/data.csv", index=False)
 print(df_list)
